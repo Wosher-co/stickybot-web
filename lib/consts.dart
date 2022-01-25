3f4b891c36f0feabb0d907345e5eacf3df7bd24f
Dolphln <dolphingamerytof@gmail.com>
2022-01-25 07:34:04 -0800

    Made base project

---
+++ lib/consts.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static const Color gradient1 = Color.fromARGB(255, 0, 47, 85);
+  static const Color gradient2 = Color.fromARGB(255, 0, 153, 255);
+  static const Color textColor = Color.fromARGB(255, 255, 255, 255);
+  static const Color buttonColor = Color.fromARGB(255, 11, 106, 122);
+}
---
+++ lib/pages/referal.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ReferalPage extends StatelessWidget {
+  final String refCode;
+
+  const ReferalPage({Key? key, this.refCode = "NONE"}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Column(
+          children: [
+            Text("Referal Page"),
+            Text(this.refCode),
+          ],
+        ),
+      ),
+    );
+  }
+}
