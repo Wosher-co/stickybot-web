39b40d616cf1f1c94f2031c55dd28f71127115e7
Pau Sevilla <pau.sevilla@icloud.com>
2022-02-14 19:09:01 +0100

    simplificate de constant code

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Constants {
-  static const Color gradient1 = Color.fromARGB(255, 0, 47, 85);
-  static const Color gradient2 = Color.fromARGB(255, 0, 153, 255);
-  static const Color textColor = Color.fromARGB(255, 255, 255, 255);
-  static const Color buttonColor = Color(0xff128C7E);
-  static const Color whatsappColor = Color(0xff25D366);
-}
+const Color gradient1 = Color.fromARGB(255, 0, 47, 85);
+const Color gradient2 = Color.fromARGB(255, 0, 153, 255);
+const Color textColor = Color.fromARGB(255, 255, 255, 255);
+const Color buttonColor = Color(0xff128C7E);
+const Color whatsappColor = Color(0xff25D366);
