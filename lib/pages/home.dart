2f1b8cbc34099e2033fd1e44eadf8336c92604c3
Pau Sevilla <pau.sevilla@icloud.com>
2022-02-14 19:05:05 +0100

    little changes

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -14,88 +14,54 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    return Scaffold(
-      body: Stack(
-        children: [
-          const AnimatedGradient(),
-          Column(
+    var screenSize = MediaQuery.of(context).size;
+    return Stack(children: [
+      AnimatedGradient(),
+      Scaffold(
+        backgroundColor: Colors.transparent,
+        body: PreferredSize(
+          preferredSize: Size(screenSize.width, 1000),
+          child: Column(
             children: [
               SizedBox(
-                height: height * 0.09,
+                height: screenSize.height / 7,
               ),
               Text(
                 'StickyBot',
                 style: TextStyle(
                     fontSize: 40,
-                    color: Constants.whatsappColor,
+                    color: Colors.white,
                     decoration: TextDecoration.none),
               ),
               SizedBox(
-                height: height * 0.06,
+                height: screenSize.height * 0.06,
               ),
               Text(
                 'Description. El mejor bot de todo el mundo chavalesssssssss',
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     fontSize: 20,
-                    color: Constants.whatsappColor,
+                    color: Colors.white,
                     decoration: TextDecoration.none),
               ),
               SizedBox(
-                height: height * 0.2,
+                height: screenSize.height * 0.2,
               ),
               button(),
             ],
           ),
-        ],
+        ),
+        bottomNavigationBar: downBar(),
       ),
-      bottomNavigationBar: BottomAppBar(
-          color: Colors.black,
-          child: Padding(
-            padding: EdgeInsets.all(15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                itemSocialNetwork(
-                    image: 'assets/instagram.png',
-                    name: '@woshergroup',
-                    link: 'https://www.instagram.com/woshergroup/'),
-                itemSocialNetwork(
-                    image: 'assets/twitter.png',
-                    name: '@TeamWosher',
-                    link: 'https://twitter.com/TeamWosher'),
-                itemSocialNetwork(
-                    image: 'assets/facebook.png',
-                    name: '@TeamWosher',
-                    link: 'https://twitter.com/TeamWosher'),
-                itemSocialNetwork(
-                    image: 'assets/linkedin.png',
-                    name: 'Wosher Group',
-                    link:
-                        'https://www.linkedin.com/in/wosher-group-4a760622b/'),
-                itemSocialNetwork(
-                    image: 'assets/youtube.png',
-                    name: 'Wosher Group',
-                    link:
-                        'https://www.youtube.com/channel/UCX0YeP722Jv1qfYhzeYs_4A'),
-                itemSocialNetwork(
-                    image: 'assets/tiktok.png',
-                    name: '@woshergroup',
-                    link: 'https://www.tiktok.com/@woshergroup'),
-              ],
-            ),
-          )),
-    );
+    ]);
   }
 
   Widget button() {
-    double height = MediaQuery.of(context).size.height;
-    double widht = MediaQuery.of(context).size.width;
+    var screenSize = MediaQuery.of(context).size;
     return Center(
       child: SizedBox(
-        width: widht * 0.156,
-        height: height * 0.099,
+        width: screenSize.width * 0.135,
+        height: screenSize.height * 0.07,
         child: ElevatedButton(
           style: ElevatedButton.styleFrom(
             shape: const RoundedRectangleBorder(
@@ -106,7 +72,7 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
           child: const Text(
             'ADD',
             style: TextStyle(
-              fontSize: 22,
+              fontSize: 14,
             ),
           ),
           onPressed: () {},
@@ -117,27 +83,86 @@ class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
 
   Widget itemSocialNetwork(
       {String image = '', String name = '', String link = ''}) {
-    double width = MediaQuery.of(context).size.width;
+    return InkWell(
+      onTap: () => launch(link),
+      child: Row(
+        children: [
+          Image.asset(
+            image,
+            // width: screenSize.width * 0.02,
+            // height: screenSize.height * 0.04,
+            width: 37,
+            height: 40,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget separator() {
+    var screenSize = MediaQuery.of(context).size;
 
     return Row(
       children: [
-        Image.asset(
-          image,
-          width: 40,
+        SizedBox(
+          width: screenSize.width * 0.004,
+        ),
+        Text(
+          '·',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+          ),
         ),
         SizedBox(
-          width: width * 0.005,
+          width: screenSize.width * 0.004,
         ),
-        InkWell(
-            child: Text(
-              name,
-              style: TextStyle(color: Colors.white, fontSize: 20),
-            ),
-            onTap: () => launch(link)),
-        SizedBox(
-          width: width * 0.015,
-        )
       ],
+    );
+  }
+
+  Widget downBar() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+            topRight: Radius.circular(12), topLeft: Radius.circular(12)),
+        color: Colors.black45,
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          itemSocialNetwork(
+              image: 'assets/instagram.png',
+              name: '@woshergroup',
+              link: 'https://www.instagram.com/woshergroup/'),
+          separator(),
+          itemSocialNetwork(
+              image: 'assets/twitter.png',
+              name: '@TeamWosher',
+              link: 'https://twitter.com/TeamWosher'),
+          separator(),
+          itemSocialNetwork(
+              image: 'assets/facebook.png',
+              name: '@TeamWosher',
+              link: 'https://twitter.com/TeamWosher'),
+          separator(),
+          itemSocialNetwork(
+              image: 'assets/linkedin.png',
+              name: 'Wosher Group',
+              link: 'https://www.linkedin.com/in/wosher-group-4a760622b/'),
+          separator(),
+          itemSocialNetwork(
+              image: 'assets/youtube.png',
+              name: 'Wosher Group',
+              link: 'https://www.youtube.com/channel/UCX0YeP722Jv1qfYhzeYs_4A'),
+          separator(),
+          itemSocialNetwork(
+              image: 'assets/tiktok.png',
+              name: '@woshergroup',
+              link: 'https://www.tiktok.com/@woshergroup'),
+        ],
+      ),
     );
   }
 }
